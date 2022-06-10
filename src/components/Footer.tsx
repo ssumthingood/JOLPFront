fa13112ab9f79db1db64c4c47f2f3a7b19c5bdb4
ssumthingood <ssumthingood@gmail.com>
2022-06-10 19:53:38 +0900

    0610

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const FooterMain = styled.div`
-    min-width:1500px;
-    height:200px;
-    padding:30px;
-    background-color:#81c147;
-    color:whitesmoke;
-    opacity:0.95;
+    min-width: 1500px;
+    height: 200px;
+    padding: 30px;
+    background-color: #81c147;
+    color: whitesmoke;
+    opacity: 0.95;
 `;
 
 const FooterWrapper = styled.div`
-    margin:0;
+    margin: 0;
 `;
 
 const HeadLine1 = styled.h1`
-  display:block;
-  font-size:25px;
-  width:100px;
+    display: block;
+    font-size: 25px;
+    width: 100px;
 `;
 
-const Footer:React.FC = () => {
-    return(
+const Footer: React.FC = () => {
+    return (
         <FooterMain>
             <FooterWrapper>
-            <HeadLine1>HoomBa</HeadLine1>
-            <div>2022 HoomBa. all right reserved</div>
-            <div>Konkuk Univ. Computer Science Engineering</div>
-            <div>Jang TaeJun, Lee KyungHo, Park JunHong</div>
+                <HeadLine1>TETA</HeadLine1>
+                <div>2022 HoomBa. all right reserved</div>
+                <div>Konkuk Univ. Computer Science Engineering</div>
+                <div>Jang TaeJun, Lee KyungHo, Park JunHong</div>
             </FooterWrapper>
-        </FooterMain> 
-    )
+        </FooterMain>
+    );
 };
 
 export default Footer;
